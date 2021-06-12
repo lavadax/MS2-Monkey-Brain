190a8a472a7b66e30c898fa600edb094338bbd71
Kevin Schepers <gamer.mmo@gmail.com>
2021-06-12 21:45:01 +0000

    Finish collisionCheck + add comment on coordList array

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,7 @@ const svgNS = "http://www.w3.org/2000/svg"
 
 let xCoord;
 let yCoord;
-let coordList = [];
+let coordList = []; /* array will contain previously used coords formatted as a string with / between X and Y coords eg: ["28/74","134/42"] */
 function testCircle() { /* TODO: Delete when done /// As the name suggests: this is a test function that will stay as a reference until I flesh out the actual function (less alt-tabbing) */
     let circle = document.createElementNS(svgNS, "circle");
     circle.setAttribute("fill", "white");
@@ -21,7 +21,15 @@ function addToArray() { /* Add coords to array after checking for collision */
 }
 
 function collisionCheck() { /* Check for collision between new coords and existing coords in array */
-
+    let col = FALSE; /* initialize as FALSE to show no collision by default (needed for first coords) */
+    coordList.forEach(function(coords) {
+        let oldCoords = coords.split("/"); /* refresher on split function taken through w3schools.com */
+        if (xCoord >= (oldCoords[0] - 45) && xCoord <= (oldCoords[0] + 45) && yCoord >= (oldCoords[1] - 45) && yCoord <= (oldCoords[1] + 45)) { 
+            /* This if was made by tweaking the temporary check that can be found in my js psuedo file */
+            col = TRUE;
+        }
+    })
+    return col;
 }
 
 function createCoords() { /* Generate coords to determine center point of circle elements */
