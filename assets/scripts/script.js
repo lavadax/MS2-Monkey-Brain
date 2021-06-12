8ce9ccc1413f8b274ba084101fb6feb712f6dff2
Kevin Schepers <gamer.mmo@gmail.com>
2021-06-12 22:22:05 +0000

    Finish drawCircles() + Add new TODO comment

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,7 @@ const svgNS = "http://www.w3.org/2000/svg"
 
 let xCoord;
 let yCoord;
-let coordList = []; /* array will contain previously used coords formatted as a string with / between X and Y coords eg: ["28/74","134/42"] */
+let coordList; /* array will contain previously used coords formatted as a string with / between X and Y coords eg: ["28/74","134/42"] */
 function testCircle() { /* TODO: Delete when done /// As the name suggests: this is a test function that will stay as a reference until I flesh out the actual function (less alt-tabbing) */
     let circle = document.createElementNS(svgNS, "circle");
     circle.setAttribute("fill", "white");
@@ -12,12 +12,21 @@ function testCircle() { /* TODO: Delete when done /// As the name suggests: this
     document.getElementsByTagName("svg")[0].appendChild(circle);
 }
 
-function addNumber() { /* Add relevant number to the circle elements based on the coords in the array and their index*/
-
+function addNumber() { /* Add relevant number to the circle elements based on the coords in the array and their index. */
+                        
 }
 
-function drawCircles() { /* Draw circle elements inside SVG*/ 
-
+function drawCircles() { /* Draw circle elements inside SVG */ 
+                        /* Code is a slightly modified version of code found on riptutorials.com (link in readme) */
+    coordList.foreEach(function(coords) {
+        let coordPair = coords.split("/"); /* Take current coord pair, number before / is first in array, number after / is second in array */
+        let circle = document.createElementNS(svgNS, "circle"); 
+        circle.setAttribute("fill", "white");
+        circle.setAttribute("cx", coordPair[0]); /* set x coord of circle's center to randomly generated x coord, relative to the svg element's top-left corner */
+        circle.setAttribute("cy", coordPair[1]); /* set y coord of circle's center to randomly generated y coord, relative to the svg element's top-left corner */
+        circle.setAttribute("r", 20); /* set circle radius to 20px */
+        $("#game-area").append(circle);
+    })
 }
 
 function addToArray() { /* Add coords to array after checking for collision */
@@ -48,3 +57,4 @@ function createCoords() { /* Generate coords to determine center point of circle
     }
 }
 
+/* TODO add main function which clears out variables at the start and calls other functions in the right order */
